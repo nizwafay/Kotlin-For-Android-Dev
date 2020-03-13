32ceeffeec92a547b9e20eea4a730be6a6d09bba
Muhammad Nizwa <muhammad.nizwa@gmail.com>
2020-03-13 23:22:18 +0700

    create recycler view

---
+++ app/src/main/java/com/example/kotlinforandroiddev/ForecastListAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinforandroiddev
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ForecastListAdapter(private val items: List<String>):
+        RecyclerView.Adapter<ForecastListAdapter.ForecastViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastViewHolder {
+        return ForecastViewHolder(TextView(parent.context))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    class ForecastViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+}
--- app/src/main/java/com/example/kotlinforandroiddev/MainActivity.kt
+++ app/src/main/java/com/example/kotlinforandroiddev/MainActivity.kt
@@ -2,13 +2,34 @@ package com.example.kotlinforandroiddev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+
+    private val items = listOf(
+        "Mon 6/23 - Sunny - 31/17",
+        "Tue 6/24 - Foggy - 21/8",
+        "Wed 6/25 - Cloudy - 22/17",
+        "Thurs 6/26 - Rainy - 18/11",
+        "Fri 6/27 - Foggy - 21/10",
+        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+        "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = ForecastListAdapter(items)
+
+        recyclerView = findViewById<RecyclerView>(R.id.forecast_list).apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 }
